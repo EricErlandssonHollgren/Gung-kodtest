550cc5665ca29cc0364bd1f9d1bbdea38ce60af8
Eric Erlandsson Hollgren <eric.e.hollgren@gmail.com>
2024-02-19 21:55:32 +0100

    Done 2

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,30 +4,25 @@ import { Product } from '../../services/product.service';
 import { ListItemComponent } from '../list-item/list-item.component';
 import { DataService } from '../../services/data.service';
 import { FilterService } from '../../services/filter.service';
-import { Observable, lastValueFrom } from 'rxjs';
-import { SkeletonLoaderComponent } from '../skeleton-loader/skeleton-loader.component';
+import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [ScrollingModule, ListItemComponent, SkeletonLoaderComponent, CommonModule],
+  imports: [ScrollingModule, ListItemComponent, CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  products: Product[];
   dataService: DataService;
   filterService: FilterService;
   filteredProducts$: Observable<Product[]>;
-  public isLoading: boolean = true;
   
   //Dependency injection of ProductService
   constructor(dataService: DataService, filterService: FilterService) {
     this.dataService = dataService;
     this.filterService = filterService;
-    this.products = [];
-
     this.filteredProducts$ = this.filterService.applyFilters(this.dataService.productDict, this.dataService.getCategoryDict());
   }
 }
